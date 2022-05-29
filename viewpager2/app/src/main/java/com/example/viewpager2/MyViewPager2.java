68d4e819762516082d5014cec9d8144c001eb0a5
jihyeon <pjh4794@gmail.com>
2022-05-30 00:07:15 +0900

    ViewPager2로 슬라이딩 이미지 구현 / indicator 추가 중

--- viewpager2/app/src/main/java/com/example/viewpager2/MyViewPager2.java
+++ viewpager2/app/src/main/java/com/example/viewpager2/MyViewPager2.java
@@ -1,14 +1,34 @@
 package com.example.viewpager2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.example.viewpager2.adapter.ViewPagerAdapter;
+
+import java.util.ArrayList;
+
 public class MyViewPager2 extends AppCompatActivity {
+
+    private ViewPager2 imageSlider;
+    private ArrayList<String> images = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_view_pager2);
+
+        images.add("https://cdn.pixabay.com/photo/2019/11/23/03/08/valley-4646114_960_720.jpg");
+        images.add("https://cdn.pixabay.com/photo/2019/04/08/16/49/blooming-4112348_960_720.jpg");
+        images.add("https://cdn.pixabay.com/photo/2021/07/19/22/07/mountains-6479150_960_720.jpg");
+        images.add("https://cdn.pixabay.com/photo/2019/06/24/16/43/mountain-4296464_960_720.jpg");
+        images.add("https://cdn.pixabay.com/photo/2016/10/25/11/37/snow-1768544__340.jpg");
+        images.add("https://cdn.pixabay.com/photo/2019/07/21/18/30/mountain-4353332_960_720.jpg");
+
+        imageSlider = findViewById(R.id.myViewPager2);
+        imageSlider.setOffscreenPageLimit(2);
+        imageSlider.setAdapter(new ViewPagerAdapter(this, images));
+
     }
 }
--- viewpager2/app/src/main/java/com/example/viewpager2/adapter/ViewPagerAdapter.java
+++ viewpager2/app/src/main/java/com/example/viewpager2/adapter/ViewPagerAdapter.java
@@ -1,2 +1,62 @@
-package com.example.viewpager2.adapter;public class ViewPagerAdapter {
+package com.example.viewpager2.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.viewpager2.R;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<String> slideImage;
+
+    public ViewPagerAdapter(Context context, ArrayList<String> slideImage) {
+        this.context = context;
+        this.slideImage = slideImage;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.item_image_slider2, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bindSlideImage(slideImage.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return slideImage.size();
+    }
+
+    // 뷰 홀더 내부 클래스 생성 (findViewById() 여기서 처리)
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.itemImageSlider);
+        }
+
+        public void bindSlideImage(String imageURL) {
+            Glide.with(context)
+                    .load(imageURL)
+                    .centerCrop()
+                    .into(imageView);
+        }
+    }
 }
