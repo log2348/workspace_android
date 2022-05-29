b14819c5aa8d5bec474c5f08c56cb92dc24afb85
jihyeon <pjh4794@gmail.com>
2022-05-29 18:24:43 +0900

    ViewPager, TabLayout으로 스와이프 뷰 구현

--- tablayout_pager/app/src/main/java/com/example/tablayout_page/MainActivity.java
+++ tablayout_pager/app/src/main/java/com/example/tablayout_page/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
--- tablayout_pager/app/src/main/java/com/example/tablayout_page/ViewPagerActivity.java
+++ tablayout_pager/app/src/main/java/com/example/tablayout_page/ViewPagerActivity.java
@@ -1,14 +1,28 @@
 package com.example.tablayout_page;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.tablayout_page.adapter.MyPagerAdapter;
+
 public class ViewPagerActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private MyPagerAdapter myPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_pager);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+
+        myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager(), 3);
+
+        viewPager.setAdapter(myPagerAdapter);
+
     }
+
 }
